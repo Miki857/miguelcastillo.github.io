c2d4c8765ecf102e7e3202e03ecc593c97e52e85
Miguel <miguelcastillov175@gmail.com>
2024-03-14 18:32:45 -0500

    Commit 2 Adding All Portfolio section and reduce img sizes

---
+++ assets/js/showPortfolio.js
@@ -0,0 +1,6 @@
+let btn_allPortfolio = document.querySelector('.section__allPortfolio')
+
+function showAllPortfolio(){
+    btn_allPortfolio.classList.toggle('allPortfolio-hidden');
+    console.log(btn_allPortfolio);
+}
---
+++ assets/js/typeEffect.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed(".auto-type", {
+    strings: ["<i class='bx bxs-chevron-left' ></i>/Miguel Castillo<i class='bx bxs-chevron-right' ></i>", 'NO ES UN BOT'],
+    typeSpeed: 50,
+    backSpeed: 150,
+    loop: true
+});
